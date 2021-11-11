3de3a9577f7b2dcb135be4c426fa6eb3230bf033
Hong Seok In <hrpt8@naver.com>
2021-11-11 20:50:45 +0900

    Update KAKAOpay Ready API

--- src/main/java/com/example/demo/src/oAuthLogin/oAuthLoginDao.java
+++ src/main/java/com/example/demo/src/oAuthLogin/oAuthLoginDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.src.oAuthLogin;
 
+import com.example.demo.src.oAuthLogin.model.KakaoPayReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,33 @@ public class oAuthLoginDao {
     @Autowired
     public void setDataSource(DataSource dataSource){
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    public int checkUserIdxByOrder(int orderIndex){
+        String checkQuery = "SELECT userIdx FROM OrderNow WHERE orderIndex =?;";
+        int checkParam = orderIndex;
+        return this.jdbcTemplate.queryForObject(checkQuery, int.class, checkParam);
+    }
+    public KakaoPayReq getPayment(int orderIndex){
+        String getQuery = "SELECT orderIndex, userIdx, concat(productName,' 등 ', SUM(num), ' 개') as productName, SUM((productPrice + first +second + third) * num )as sumPrice, SUM(num) as num\n" +
+                "FROM GetCart GC left join (SELECT productIdx, productName, productPrice * (100-salePercent)/100 as productprice FROM Product) as P on P.productIdx = GC.productIdx\n" +
+                "                left join (SELECT optionIdx, optionPrice as first FROM ProductFirstOption) as PFO on GC.firstOptionIdx = PFO.optionIdx\n" +
+                "                left join (SELECT secondOptionIdx, optionPrice as second FROM ProductSecondOption) as PSO on GC.secondOptionIdx = PSO.secondOptionIdx\n" +
+                "                left join (SELECT thirdOptionIdx, optionPrice as third FROM ProductThirdOption) as PTO on GC.thirdOptionIdx = PTO.thirdOptionIdx\n" +
+                "                left join (SELECT orderIndex, cartIdx, userIdx FROM OrderNow) as O on GC.cartIdx = O.cartIdx\n" +
+                "WHERE orderIndex =?;";
+        int params = orderIndex;
+        return this.jdbcTemplate.queryForObject(getQuery,
+                (rs, rowNum) -> new KakaoPayReq(
+                        "TC0ONETIME",
+                        Integer.toString(rs.getInt("orderIndex")),
+                        Integer.toString(rs.getInt("userIdx")),
+                        rs.getString("productName"),
+                        rs.getInt("num"),
+                        rs.getInt("sumPrice"),
+                        0,
+                        "https://prod.seokin-test.shop",
+                        "https://prod.seokin-test.shop",
+                        "https://prod.seokin-test.shop"
+                ), params);
     }
 }
--- src/main/java/com/example/demo/src/oAuthLogin/oAuthLoginService.java
+++ src/main/java/com/example/demo/src/oAuthLogin/oAuthLoginService.java
@@ -1,13 +1,16 @@
 package com.example.demo.src.oAuthLogin;
 
 import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponseStatus;
 import com.example.demo.config.secret.Secret;
-import com.example.demo.src.oAuthLogin.model.KakaoUserInfo;
-import com.example.demo.src.oAuthLogin.model.KakaoUserNameReq;
+import com.example.demo.src.oAuthLogin.model.*;
 import com.example.demo.src.user.model.PostLoginRes;
 import com.example.demo.src.user.model.PostUserReq;
 import com.example.demo.utils.AES128;
 import com.example.demo.utils.JwtService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import com.example.demo.src.user.*;
@@ -38,12 +42,110 @@ public class oAuthLoginService {
 
     }
 
+    // 카카오 액세스토큰 받기위해 구현
+    public String getKakaoToken(String code){
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "7f5bb71f045567209d1beb4e84d91b0c");
+        params.add("redirect_uri", "http://localhost:8080/ohouse/kakao/auth/kakao/callback");
+        params.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = new OAuthToken();
+        try {
+            oauthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
+        } catch (JsonMappingException exception) {
+            exception.printStackTrace();
+        } catch (JsonProcessingException exception) {
+            exception.printStackTrace();
+        }
+        return oauthToken.getAccess_token();
+    }
+    public KakaoPayReq kakaoPayReq(int orderIndex, int userIdx) throws BaseException {
+        try {
+            if (oAuthLoginDao.checkUserIdxByOrder(orderIndex) != userIdx) {
+                throw new BaseException(INVALID_USER_JWT);
+            }
+            KakaoPayReq kakaoPayReq = oAuthLoginDao.getPayment(orderIndex);
+            return kakaoPayReq;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+    public String payReady(int orderIndex, int userIdx) throws BaseException{
+        try
+        {
+            if(oAuthLoginDao.checkUserIdxByOrder(orderIndex)!= userIdx){
+                throw new BaseException(INVALID_USER_JWT);
+            }
+            KakaoPayReq kakaoPayReq = oAuthLoginDao.getPayment(orderIndex);
+
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","KakaoAK 3c4dd8f4914bbcd64a8a47865ccf307b");
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("cid", kakaoPayReq.getCid());
+        params.add("partner_order_id", kakaoPayReq.getPartner_order_id());
+        params.add("partner_user_id", kakaoPayReq.getPartner_user_id());
+        params.add("item_name",kakaoPayReq.getItem_name());
+        params.add("quantity",Integer.toString(kakaoPayReq.getQuantity()));
+        params.add("total_amount",Integer.toString(kakaoPayReq.getTotal_amount()));
+        params.add("tax_free_amount",Integer.toString(kakaoPayReq.getTax_free_amount()));
+        params.add("approval_url",kakaoPayReq.getApproval_url());
+        params.add("cancel_url", kakaoPayReq.getCancel_url());
+        params.add("fail_url",kakaoPayReq.getFail_url());
+
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
+                new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = rt.exchange(
+                "https://kapi.kakao.com/v1/payment/ready",
+                HttpMethod.POST,
+                kakaoTokenRequest,
+                String.class
+        );
+
+
+            KakaoPayRes kakaoPayRes = new KakaoPayRes();
+            ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            kakaoPayRes = objectMapper.readValue(response.getBody(), KakaoPayRes.class);
+        } catch (JsonMappingException exception) {
+            exception.printStackTrace();
+        } catch (JsonProcessingException exception) {
+            exception.printStackTrace();
+        }
+        return kakaoPayRes.getNext_redirect_pc_url();
+        }
+        catch(BaseException e){
+            throw new BaseException(e.getStatus());
+        }
+        catch(Exception exception){
+            System.err.println(exception.toString());
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
     public KakaoUserInfo getKakaoUserInfo(String accessToken){
         RestTemplate rt2 = new RestTemplate();
         HttpHeaders headers2 = new HttpHeaders();
         headers2.add("Authorization","Bearer " + accessToken);
         headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         HttpEntity<MultiValueMap<String, String>> kakaoUserInfo =
                 new HttpEntity<>(headers2);
         ResponseEntity<KakaoUserInfo> response2 = rt2.exchange(
